4d61f08d3540576177471a878457171819a743ed
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2025-08-13 15:09:11 +0530

    Add AverageOfThreeNumbers

---
+++ AverageOfThreeNumbers.java
@@ -0,0 +1,9 @@
+public class AverageOfThreeNumbers {
+     public static void main(String[] args) {
+        double num1 = 10.0, num2 = 20.0, num3 = 30.0;
+        
+        double average = (num1 + num2 + num3) / 3;
+
+        System.out.println("Average of three numbers: " + average);
+    }
+}
