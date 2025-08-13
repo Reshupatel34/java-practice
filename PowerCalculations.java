20c275e424a8b63ea03ecabce80dab010b984d59
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2025-08-13 15:07:25 +0530

    Add PowerCalculation

---
+++ PowerCalculations.java
@@ -0,0 +1,12 @@
+public class PowerCalculations {
+    
+
+    public static void main(String[] args) {
+        double base = 2.0; 
+        double exponent = 3.0; 
+
+        double power = Math.pow(base, exponent);
+
+        System.out.println("Power: " + power);
+    }
+}
