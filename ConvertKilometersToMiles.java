93079c92218b1b7929bde6b6a5e3d2be1ae95d6f
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2025-08-13 15:11:23 +0530

    Add ConvertKilometersToMiles

---
+++ ConvertKilometersToMiles.java
@@ -0,0 +1,8 @@
+public class ConvertKilometersToMiles {
+    public static void main(String[] args) {
+        double kilometers = 10.0; 
+        double miles = kilometers * 0.621371; 
+
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+}
