3f3045ba2b07cc2258f898fbc44a3ec8c9c3bb0b
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2025-08-14 14:50:57 +0530

    Add SAmple_problem_1

---
+++ competitive_programming/sample_problem_1.java
@@ -0,0 +1,14 @@
+package competitive_programming;
+
+public class sample_problem_1 {
+    public static void main(String[] args) {
+        String name="Sam";
+       int rollNumber = 1;
+       double percentMarks = 99.99;
+       char result = 'P';
+        System.out.println("Displaying Result:\n" +name+ " with Roll Number " +
+rollNumber+ " has Scored " +percentMarks+ 
+"% Marks and Result is " +result);
+
+    }
+}
